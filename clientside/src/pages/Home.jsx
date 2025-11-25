a8e3e8c40098addefd4c1c33a61242cea28d033c
MuneebAhmed01 <imuneebahmed0@gmail.com>
2025-11-25 14:48:13 +0500

    .

--- clientside/src/pages/Home.jsx
+++ clientside/src/pages/Home.jsx
@@ -3,14 +3,25 @@ import Headers from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import TopDoctors from '../components/TopDoctors'
 import Banner from '../components/Banner'
+import FeaturesSlider from '../components/FeaturesSlider'
+import Working from '../components/Working'
+import HealthTips from '../components/HealthTips'
+
+import FAQSection from '../components/Faq'
 
 const Home = () => {
   return (
     <div>
       <Headers />
+      <FeaturesSlider/>
+      
+      <Working/>
+      <HealthTips/>
+      
       <SpecialityMenu />
       <TopDoctors />
-      <Banner />
+      {/* <Banner /> */}
+      <FAQSection/>
     </div>
   )
 }
