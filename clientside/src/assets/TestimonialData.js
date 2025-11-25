75613bb0af57e3060735fbf27d6750049c037784
MuneebAhmed01 <imuneebahmed0@gmail.com>
2025-11-25 14:47:22 +0500

    .

---
+++ clientside/src/assets/TestimonialData.js
@@ -0,0 +1,48 @@
+import { assets } from "./assets";
+
+
+
+export const testimonialData = [
+{
+name: "Ibrahimuix",
+image:  assets.testi2,
+text:
+"Very effective service from everyone, bright cheerful staff. Wonderful service from everyone. Fabulous team, we really appreciate...",
+},
+{
+name: "Dezzo Farhan",
+image:  assets.testi3,
+text:
+"Staff are exceptional, very courteous and efficient, maintaining a sense of humour. The GP I saw was very professional...",
+},
+{
+name: "Sarah Khan",
+image: assets.testi4,
+text:
+"Amazing consultation experience. Doctors listened carefully and guided me through the recovery process...",
+},
+{
+name: "Umer Tariq",
+image: assets.testi1,
+text:
+"Really smooth booking experience and the environment was really comforting. Highly recommended...",
+},
+{
+name: "Maya Hassan",
+image: assets.testi2,
+text:
+"Honestly the best service I’ve received. Everything felt organised and efficient...",
+},
+{
+name: "David Noor",
+image:  assets.testi3,
+text:
+"Great staff and clean environment. My appointment went smoothly without any delays...",
+},
+{
+name: "Amna Raza",
+image:  assets.testi4,
+text:
+"The doctor explained everything in detail and made sure all my concerns were answered...",
+},
+];
