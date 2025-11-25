da4191eb28ff63d1974994eb3e788d9ff4b9c028
MuneebAhmed01 <imuneebahmed0@gmail.com>
2025-11-25 14:46:55 +0500

    .

--- clientside/tailwind.config.js
+++ clientside/tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // make sure paths match your project
+  ],
   theme: {
     extend: {
       colors: {
         primary: "#5f6FFF",
+      },
+      animation: {
+        gradient: "gradientMove 8s ease infinite",
+      },
+      keyframes: {
+        gradientMove: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
+      },
+      backgroundSize: {
+        "200": "200% 200%",
       },
       gridTemplateColumns: {
         auto: "repeat(auto-fill, minmax(200px, 1fr))",
