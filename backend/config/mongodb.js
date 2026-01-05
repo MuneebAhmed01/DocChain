c7e18f05714329f8243745e66a50f4a9e9e68d88
MuneebAhmed01 <imuneebahmed0@gmail.com>
2026-01-05 17:35:42 +0500

    .

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,9 +1,17 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => console.log("Database Connected"));
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Database Connected");
+    });
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/DocChain`);
+    await mongoose.connect(process.env.MONGODB_URI);
+  } catch (error) {
+    console.log(process.env.MONGODB_URI)
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
